a6bd6f5d8ccc0059d3f1250d3cc074861e0f0a81
Richard Redfern <richardred15@gmail.com>
2020-04-20 16:54:51 -0600

    Mild Documentation

--- src/database.js
+++ src/database.js
@@ -5,6 +5,12 @@ const Errors = require("./error");
 let Configuration = require("./configuration");
 
 class Database {
+    /**
+     * 
+     * @param {string} directory 
+     * @param {string} password?
+     * @description Creates a new Database object
+     */
     constructor(directory, password = 0) {
         this.key = password;
         if (this.key != 0) {
@@ -23,6 +29,9 @@ class Database {
         }
     }
 
+    /**
+     * @description Load configuration and table manager
+     */
     load() {
         if (this.key != 0) {
             Configuration.key = this.key;
@@ -34,6 +43,9 @@ class Database {
         this.initialized = true;
     }
 
+    /**
+     * @description Initialize a new database
+     */
     initialize() {
         if (!this.initialized) {
             fs.mkdirSync(this.directory);
@@ -54,6 +66,11 @@ class Database {
         }
     }
 
+    /**
+     * 
+     * @param {string} name 
+     * @description Set active table
+     */
     selectTable(name) {
         if (this.table_manager.exists(name)) {
             this.current_table_name = name;
@@ -62,22 +79,45 @@ class Database {
             throw new Errors.NoSuchTableError();
     }
 
+    /**
+     * 
+     * @param {string} name 
+     * @param {string} term 
+     * @description Search a specified column for a specified term
+     */
     searchColumn(name, term) {
         return this.table_manager.searchColumn(this.current_table_name, name, term);
     }
 
+    /**
+     * @param {any[]} args
+     * @description Insert row containing specified values
+     */
     insertRow() {
         this.table_manager.insertRow(this.current_table_name, arguments);
     }
 
+    /**
+     * 
+     * @param {object} newData {column:name,data:value}
+     * @param {object} where {column:term}
+     */
     updateRows(newData, where) {
         this.table_manager.updateRows(this.current_table_name, newData, where);
     }
 
+    /**
+     * @description List columns from selected table
+     */
     tableColumns() {
         return this.table_manager.getColumns(this.current_table_name);
     }
 
+    /**
+     * 
+     * @param {string} name?
+     * @description Get all rows from selected or indicated table
+     */
     getRows(name = this.current_table_name) {
         return this.table_manager.getRows(name);
     }
@@ -96,6 +136,12 @@ class Database {
 
     }
 
+    /**
+     * 
+     * @param {string} name 
+     * @param {string[]} columns 
+     * @description Create a new table with specified columns
+     */
     createTable(name, columns) {
         if (!this.initialized) {
             throw new Errors.DatabaseNotInitializedError();
